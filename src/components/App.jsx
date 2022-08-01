32559434652a11f5e6aeee46820ef1f2c7e02487
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-08-01 15:24:18 +0300

    hw-5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home } from 'path/to/pages/Home';
+// import { Movies } from 'path/to/pages/Movies';
+// import { NotFound } from 'path/to/pages/NotFound';
+// import { MoviesDetails } from 'path/to/pages/MoviesDetails';
+// import { NavLink } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/Movies">Movies</NavLink>
+        {/* <Link to="/products/h-1" state={{ from: "/dashboard?name=hoodie" }}> через state бросам на кнопку путь назад анпример в /product*/}
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>}></Route>
+        {/* <Route path="/Movies" element={<Movies />} />
+        <Route path="/Movies/:id" element={<MoviesDetails />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
     </div>
   );
 };
